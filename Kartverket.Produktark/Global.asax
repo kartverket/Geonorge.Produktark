e5f5e54a6ac191b09c501bcdee25e43e7d4a6663
Henning Jensen <henning@arkitektum.no>
2014-09-29 11:51:22 +0200

    Blank ASP MVC project

---
+++ Kartverket.Produktark/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kartverket.Produktark.MvcApplication" Language="C#" %>
---
+++ Kartverket.Produktark/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kartverket.Produktark.Startup))]
+namespace Kartverket.Produktark
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
