0890b72f811b3bc8417ec88f5c68e2a9c6030777
Henning Jensen <henning@arkitektum.no>
2014-10-09 15:15:54 +0200

    Add form fields for the additional fields not coming from metadata.

--- Kartverket.Produktark/Controllers/ProductSheetsController.cs
+++ Kartverket.Produktark/Controllers/ProductSheetsController.cs
@@ -79,48 +79,18 @@ namespace Kartverket.Produktark.Controllers
             return View(productSheet);
         }
 
-        // POST: ProductSheets/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Uuid,Title,Description")] ProductSheet productSheet)
+        public ActionResult Edit(ProductSheet productSheet)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(productSheet).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("CreatePdf", new { id = productSheet.Id });
             }
             return View(productSheet);
         }
-
-        // GET: ProductSheets/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ProductSheet productSheet = db.ProductSheet.Find(id);
-            if (productSheet == null)
-            {
-                return HttpNotFound();
-            }
-            return View(productSheet);
-        }
-
-        // POST: ProductSheets/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            ProductSheet productSheet = db.ProductSheet.Find(id);
-            db.ProductSheet.Remove(productSheet);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
 
         public ActionResult CreatePdf(int? id)
         {
