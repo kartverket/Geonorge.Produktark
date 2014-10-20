10fc0490835183935953d9ebf597747379ea0568
Dag Olav <dagolav@arkitektum.no>
2014-10-20 12:42:52 +0200

    Fix handle when contact data is null

--- Kartverket.Produktark/Models/ProductSheetService.cs
+++ Kartverket.Produktark/Models/ProductSheetService.cs
@@ -33,9 +33,9 @@ namespace Kartverket.Produktark.Models
                 productSheet.Purpose = simpleMetadata.Purpose;
                 productSheet.SpecificUsage = simpleMetadata.SpecificUsage;
                 productSheet.UseLimitations = simpleMetadata.Constraints != null ? simpleMetadata.Constraints.UseLimitations : null;
-                productSheet.ContactMetadata = simpleMetadata.ContactMetadata != null ? CreateContact(simpleMetadata.ContactMetadata) : null;
-                productSheet.ContactPublisher = simpleMetadata.ContactPublisher != null ? CreateContact(simpleMetadata.ContactPublisher) : null;
-                productSheet.ContactOwner = simpleMetadata.ContactOwner != null ? CreateContact(simpleMetadata.ContactOwner) : null;
+                productSheet.ContactMetadata = simpleMetadata.ContactMetadata != null ? CreateContact(simpleMetadata.ContactMetadata) : new Contact();
+                productSheet.ContactPublisher = simpleMetadata.ContactPublisher != null ? CreateContact(simpleMetadata.ContactPublisher) : new Contact();
+                productSheet.ContactOwner = simpleMetadata.ContactOwner != null ? CreateContact(simpleMetadata.ContactOwner) : new Contact();
                 productSheet.ResolutionScale = simpleMetadata.ResolutionScale;
                 productSheet.KeywordsPlace = CreateKeywords(SimpleKeyword.Filter(simpleMetadata.Keywords, SimpleKeyword.TYPE_PLACE, null));
                 productSheet.ProcessHistory = simpleMetadata.ProcessHistory;
@@ -65,9 +65,9 @@ namespace Kartverket.Produktark.Models
         {
             return new Contact
             {
-                Email = contact.Email,
-                Name = contact.Name,
-                Organization = contact.Organization
+                Email = contact.Email != null ? contact.Email : "" ,
+                Name = contact.Name != null ? contact.Name : "",
+                Organization = contact.Organization != null ? contact.Organization : ""
             };
         }
 
