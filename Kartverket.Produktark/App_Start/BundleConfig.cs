612d3d3f93a0cb404fb84da90c87a2e9e506f700
Benjamin Dehli <benjamin@arkitektum.no>
2016-06-07 16:41:16 +0200

    Add and include Bower components

--- Kartverket.Produktark/App_Start/BundleConfig.cs
+++ Kartverket.Produktark/App_Start/BundleConfig.cs
@@ -8,8 +8,15 @@ namespace Kartverket.Produktark
         
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                           "~/Content/custom.css"));
+            bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/scripts").Include(
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js"
+           ));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                 "~/Scripts/jquery.validate*"));
