a7713e253ba5be6230089dd21bc10cd46f82dcf9
Dag Olav Dahle <dagolav@arkitektum.no>
2023-06-01 11:10:59 +0200

    Upgrade jquery

--- Kartverket.Produktark/Scripts/_references.js
+++ Kartverket.Produktark/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.4.1.js" />
+﻿/// <reference path="jquery-3.7.0.js" />
 /// <autosync enabled="true" />
 /// <reference path="bootstrap.js" />
 /// <reference path="jquery.validate.js" />
